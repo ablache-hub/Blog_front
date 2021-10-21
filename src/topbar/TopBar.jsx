6ce3859dfa9c957acd226accbe265b810e95bedb
ablache <alexandreblache.pro@gmail.com>
2021-10-21 15:46:01 +0200

    Base

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,25 @@
+import "./topbar.css"
+
+export default function TopBar() {
+    return (
+        <div className="top">
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-twitter-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <img className="topImg" src="/img/profil.jpg"></img>
+                <i class="topSearchIcon fas fa-search"></i>
+            </div>
+        </div>
+    )
+}
