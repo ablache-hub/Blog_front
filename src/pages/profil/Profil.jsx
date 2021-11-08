159b338bfe98178777f42740a57e265796d6704d
ablache <alexandreblache.pro@gmail.com>
2021-11-08 16:54:58 +0100

    debug

--- src/pages/profil/Profil.jsx
+++ src/pages/profil/Profil.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 export default function Profil() {
     const [fetchProfil, setFetchProfil] = useState([]);
     const { token, username } = useContext(Context);
-    const [id, setId] = useState([]);
+    const [id, setId] = useState(null);
 
     useEffect(() => {
         const fetchingProfil = async () => {
@@ -26,49 +26,27 @@ export default function Profil() {
                 {
                     headers: { 'Authorization': token }
                 });
-                window.location.reload();
+            window.location.reload();
 
         }
-        deleteArticle();
+        id && deleteArticle();
 
     }, [id])
 
-    const handleClick = (event) => {
+    const getId = (event) => {
         setId(event.target.id);
     }
-
-
-    // const handleDelete = async () => {
-    //     await axios.delete("/article/auteur/" + username + "/delete/" + article.id,
-    //         {
-    //             headers: { 'Authorization': token }
-    //         });
-    // window.location.replace("/")
-    // }
-
-    // useEffect(() => {
-    //     console.log(id)
-    //     const deleteArticle = async () => {
-    //               await axios.delete("/article/auteur/" + username + "/delete/" + id,
-    //             {
-    //                 headers: { 'Authorization': token }
-    //             })
-    //     }
-    //     deleteArticle()
-    // }, [id]);
 
 
     return (
         <div>
             <span>
-                {
-                    console.log(fetchProfil),
-                    fetchProfil.articles &&
+                {fetchProfil.articles &&
                     fetchProfil.articles.map((article) =>
-                        <ul className="article">
+                        <ul className="article" key={article.id}>
                             <i className="singlePostIcon far fa-edit"></i>
                             <i className="singlePostIcon far fa-trash-alt"
-                                id={article.id} onClick={handleClick}
+                                id={article.id} onClick={getId}
                             ></i>
                             <li>Titre: {article.titre}</li>
                             <li>Date: {article.date}</li>
