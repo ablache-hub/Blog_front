fe1f552be653f3c3cfd75382fcbcc287492a2c2e
ablache <alexandreblache.pro@gmail.com>
2021-12-01 17:30:52 +0100

    path_test

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "build")));
 
 // // This route serves the React app
-// app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
