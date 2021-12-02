ecf2f50516756f76ff127d53a01654580f2c1ee0
ablache <alexandreblache.pro@gmail.com>
2021-12-02 11:27:49 +0100

    test_routes

--- server.js
+++ server.js
@@ -13,10 +13,5 @@ app.get('*', (request, response) => {
 	response.sendFile(path.join(__dirname + 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
